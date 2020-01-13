e29c1965c2b0e0488b17b4a911838d46e096efa1
Vinicius <vm.murad@gmail.com>
2020-01-13 17:36:58 -0300

    add function to format price

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,100 +1,44 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { MdAddShoppingCart } from 'react-icons/md';
+import { formatPrice } from '../../util/format';
+import api from '../../services/api';
 
 import { ProductList } from './styles';
 
-export default function Home() {
-    return (
-        <ProductList>
-            <li>
-                <img
-                    src="https://static.netshoes.com.br/produtos/tenis-asics-gel-flux-5-a-masculino/72/D18-2439-172/D18-2439-172_zoom1.jpg"
-                    alt="Shoes"
-                />
-                <strong>Shoes</strong>
-                <span>R$129,90</span>
-                <button type="button">
-                    <div>
-                        <MdAddShoppingCart size={16} color="#FFF" />3
-                    </div>
-                    <span>ADD TO CART</span>
-                </button>
-            </li>
+export default class Home extends Component {
+    state = {
+        products: [],
+    };
 
-            <li>
-                <img
-                    src="https://static.netshoes.com.br/produtos/tenis-asics-gel-flux-5-a-masculino/72/D18-2439-172/D18-2439-172_zoom1.jpg"
-                    alt="Shoes"
-                />
-                <strong>Shoes</strong>
-                <span>R$129,90</span>
-                <button type="button">
-                    <div>
-                        <MdAddShoppingCart size={16} color="#FFF" />3
-                    </div>
-                    <span>ADD TO CART</span>
-                </button>
-            </li>
+    async componentDidMount() {
+        const response = await api.get('products');
 
-            <li>
-                <img
-                    src="https://static.netshoes.com.br/produtos/tenis-asics-gel-flux-5-a-masculino/72/D18-2439-172/D18-2439-172_zoom1.jpg"
-                    alt="Shoes"
-                />
-                <strong>Shoes</strong>
-                <span>R$129,90</span>
-                <button type="button">
-                    <div>
-                        <MdAddShoppingCart size={16} color="#FFF" />3
-                    </div>
-                    <span>ADD TO CART</span>
-                </button>
-            </li>
+        const data = response.data.map(product => ({
+            ...product,
+            priceFormatted: formatPrice(product.price),
+        }));
 
-            <li>
-                <img
-                    src="https://static.netshoes.com.br/produtos/tenis-asics-gel-flux-5-a-masculino/72/D18-2439-172/D18-2439-172_zoom1.jpg"
-                    alt="Shoes"
-                />
-                <strong>Shoes</strong>
-                <span>R$129,90</span>
-                <button type="button">
-                    <div>
-                        <MdAddShoppingCart size={16} color="#FFF" />3
-                    </div>
-                    <span>ADD TO CART</span>
-                </button>
-            </li>
+        this.setState({ products: data });
+    }
 
-            <li>
-                <img
-                    src="https://static.netshoes.com.br/produtos/tenis-asics-gel-flux-5-a-masculino/72/D18-2439-172/D18-2439-172_zoom1.jpg"
-                    alt="Shoes"
-                />
-                <strong>Shoes</strong>
-                <span>R$129,90</span>
-                <button type="button">
-                    <div>
-                        <MdAddShoppingCart size={16} color="#FFF" />3
-                    </div>
-                    <span>ADD TO CART</span>
-                </button>
-            </li>
-
-            <li>
-                <img
-                    src="https://static.netshoes.com.br/produtos/tenis-asics-gel-flux-5-a-masculino/72/D18-2439-172/D18-2439-172_zoom1.jpg"
-                    alt="Shoes"
-                />
-                <strong>Shoes</strong>
-                <span>R$129,90</span>
-                <button type="button">
-                    <div>
-                        <MdAddShoppingCart size={16} color="#FFF" />3
-                    </div>
-                    <span>ADD TO CART</span>
-                </button>
-            </li>
-        </ProductList>
-    );
+    render() {
+        const { products } = this.state;
+        return (
+            <ProductList>
+                {products.map(product => (
+                    <li key={product.id}>
+                        <img src={product.image} alt={product.title} />
+                        <strong>{product.title}</strong>
+                        <span>{product.priceFormatted}</span>
+                        <button type="button">
+                            <div>
+                                <MdAddShoppingCart size={16} color="#FFF" />3
+                            </div>
+                            <span>ADD TO CART</span>
+                        </button>
+                    </li>
+                ))}
+            </ProductList>
+        );
+    }
 }
