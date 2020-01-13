e4e2cbae385837599ff26324268159886b05087a
Vinicius <vm.murad@gmail.com>
2020-01-13 16:37:41 -0300

    add style in cart page

--- src/pages/Cart/index.js
+++ src/pages/Cart/index.js
@@ -1,7 +1,73 @@
 import React from 'react';
+import {
+    MdRemoveCircleOutline,
+    MdAddCircleOutline,
+    MdDelete,
+} from 'react-icons/md';
 
-// import { Container } from './styles';
+import { Container, ProductTable, Total } from './styles';
 
 export default function Cart() {
-    return <div />;
+    return (
+        <Container>
+            <ProductTable>
+                <thead>
+                    <tr>
+                        <th />
+                        <th>PRODUCT</th>
+                        <th>QUANTITY</th>
+                        <th>SUBTOTAL</th>
+                        <th />
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>
+                            <img
+                                src="https://static.netshoes.com.br/produtos/tenis-asics-gel-flux-5-a-masculino/72/D18-2439-172/D18-2439-172_zoom1.jpg"
+                                alt="Shoes"
+                            />
+                        </td>
+                        <td>
+                            <strong> Shoes</strong>
+                            <span>$100.00</span>
+                        </td>
+                        <td>
+                            <div>
+                                <button type="button">
+                                    <MdRemoveCircleOutline
+                                        size={20}
+                                        color="#7159c1"
+                                    />
+                                </button>
+                                <input type="number" readOnly value={2} />
+                                <button type="button">
+                                    <MdAddCircleOutline
+                                        size={20}
+                                        color="#7159c1"
+                                    />
+                                </button>
+                            </div>
+                        </td>
+                        <td>
+                            <strong>$200.00</strong>
+                        </td>
+                        <td>
+                            <button type="button">
+                                <MdDelete size={20} color="#7159c1" />
+                            </button>
+                        </td>
+                    </tr>
+                </tbody>
+            </ProductTable>
+            <footer>
+                <button type="button">Go to checkout</button>
+
+                <Total>
+                    <span>Total</span>
+                    <strong>$2000.00</strong>
+                </Total>
+            </footer>
+        </Container>
+    );
 }
