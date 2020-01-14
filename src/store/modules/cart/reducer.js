91d3a979e74f4140c14d6135adee7a79f42556b4
Vinicius <vm.murad@gmail.com>
2020-01-14 17:17:58 -0300

    remove item from cart

--- src/store/modules/cart/reducer.js
+++ src/store/modules/cart/reducer.js
@@ -1,9 +1,9 @@
-import product from 'immer';
+import produce from 'immer';
 
 export default function cart(state = [], action) {
     switch (action.type) {
         case 'ADD_TO_CART':
-            return product(state, draft => {
+            return produce(state, draft => {
                 const productIndex = draft.findIndex(
                     p => p.id === action.product.id
                 );
@@ -15,6 +15,14 @@ export default function cart(state = [], action) {
                         ...action.product,
                         amount: 1,
                     });
+                }
+            });
+        case 'REMOVE_FROM_CART':
+            return produce(state, draft => {
+                const productIndex = draft.findIndex(p => p.id === action.id);
+
+                if (productIndex >= 0) {
+                    draft.splice(productIndex, 1);
                 }
             });
         default:
